7a124f871779acd92251db346e0d3f793e6ff057
fakestudent733 <108695634+fakestudent733@users.noreply.github.com>
2023-02-10 13:09:15 -0700

    start code complete

--- js/main.js
+++ js/main.js
@@ -1,1 +1,55 @@
-console.log("hi");
+// TRAVERSE DATA CYU ASSIGNMENT START CODE
+
+// Load Data From Files
+let surveyData;
+fetch("data/survey-results.txt")
+  .then((rawData) => rawData.text())
+  .then((strData) => (surveyData = strData.split(/\r?\n/)));
+
+let ageData;
+fetch("data/age-data.txt")
+  .then((rawData) => rawData.text())
+  .then((strData) => (ageData = strData.split(/\r?\n/)));
+
+let numberData;
+fetch("data/number-data.txt")
+  .then((rawData) => rawData.text())
+  .then((strData) => (numberData = strData.split(/\r?\n/)));
+
+// Output Element Variable
+let outputEl = document.getElementById("output");
+
+// Button Event Listener
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+  // Get Menu Selection
+  let selection = document.getElementById("menu-select").value;
+
+  // Process Menu Selection
+  if (selection === "survey") {
+    traverseSurveyData();
+  } else if (selection === "ages") {
+    traverseAgeData();
+  } else if (selection === "numbers") {
+    traverseNumberData();
+  }
+}
+
+// Menu Option Functions
+function traverseSurveyData() {
+  // Count the number of each response: "Yes", "No", "Maybe"
+  // and output the results.
+  outputEl.innerHTML = "Survey Data";
+  console.log(surveyData);
+}
+
+function traverseAgeData() {
+  outputEl.innerHTML = "Age Data";
+  console.log(ageData);
+}
+
+function traverseNumberData() {
+  outputEl.innerHTML = "Number Data";
+  console.log(numberData);
+}
