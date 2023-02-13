ce14bb900ac02728c280e75dc71084b42c4ac3b4
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2023-02-13 10:15:29 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -38,18 +38,34 @@ function btnClicked() {
 
 // Menu Option Functions
 function traverseSurveyData() {
-  // Count the number of each response: "Yes", "No", "Maybe"
-  // and output the results.
+  // Traverse the surveyData array to:
+  // Count the number of "Yes" responses,
+  // Count the number of "No" responses,
+  // Count the number of "Maybe" responses,
+  // and output the results in the outputEl.
+
   outputEl.innerHTML = "Survey Data";
   console.log(surveyData);
 }
 
 function traverseAgeData() {
+  // Traverse the ageData array to:
+  // Count the number of ages under 18,
+  // Count the number of ages between 18 and 35, inclusive
+  // Count the number of ages between 36 and 65, inclusive
+  // Count the number of ages above 65,
+  // and output the results in the outputEl.
+
   outputEl.innerHTML = "Age Data";
   console.log(ageData);
 }
 
 function traverseNumberData() {
+  // Traverse the numberData array to:
+  // Count the number of even numbers,
+  // Count the number of odd numbers,
+  // and output the results in the outputEl.
+
   outputEl.innerHTML = "Number Data";
   console.log(numberData);
 }
